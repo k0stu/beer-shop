8a5779d1164bd788c58185dc7bf12acd1342092c
Kostya Tarasov <k0stu@github.com>
2012-07-30 09:20:40 +0300

    S**tload of untested code

--- Zion/src/main/java/ss/bshop/CreateOrderActivity.java
+++ Zion/src/main/java/ss/bshop/CreateOrderActivity.java
@@ -4,6 +4,11 @@ import ss.bshop.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class CreateOrderActivity extends Activity {
 	private static final String TAG = "CreateOrderActivity";
@@ -12,5 +17,28 @@ public class CreateOrderActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "created");
 		setContentView(R.layout.createorder);
+		Spinner orderTypeSpinner = 
+				(Spinner) findViewById(R.id.orderTypeSpinner);
+		ArrayAdapter<CharSequence> orderTypes = ArrayAdapter
+				.createFromResource(this, R.array.orderType, 
+				android.R.layout.simple_spinner_item);
+		orderTypes.setDropDownViewResource(
+				android.R.layout.simple_spinner_dropdown_item);
+		orderTypeSpinner.setAdapter(orderTypes);
+		Button cancel = (Button) findViewById(R.id.cancelOrderButton);
+		cancel.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				CreateOrderActivity.this.finish();				
+			}
+		});
+		Button save = (Button) findViewById(R.id.saveOrderButton);
+		save.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				// TODO: create and return OutletOrderMobile object logics
+				CreateOrderActivity.this.finish();
+			}
+		});
 	}
 }
--- Zion/src/main/java/ss/bshop/CreateVisitActivity.java
+++ Zion/src/main/java/ss/bshop/CreateVisitActivity.java
@@ -1,22 +1,32 @@
 package ss.bshop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ss.bshop.R;
+import ss.bshop.communication.Communicator;
+import ss.bshop.mobile.entities.OutletMobile;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class CreateVisitActivity extends Activity {
 
 	private static final String TAG = "CreateVisitActivity";
+	private static final int ORDER = 0;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "created");
 		setContentView(R.layout.createvisit);
+		prepareSpinner();
 		Button close = (Button) findViewById(R.id.closeVisitButton);
 		close.setOnClickListener(new OnClickListener() {
 			@Override
@@ -37,7 +47,29 @@ public class CreateVisitActivity extends Activity {
 			public void onClick(View view) {
 				Intent createOrder = new Intent(CreateVisitActivity.this,
 						CreateOrderActivity.class);
+				startActivityForResult(createOrder, ORDER);
 			}
 		});
+	}
+
+	private void prepareSpinner() {
+		ArrayAdapter outletNames = new ArrayAdapter(this,
+				android.R.layout.simple_spinner_item);
+		List<OutletMobile> outlets = Communicator
+				.getOutletsForToday(Global.username);
+		for (OutletMobile outlet : outlets) {
+			outletNames.add(outlet.getName());
+		}
+		Spinner outletNamesSpinner = (Spinner) findViewById(R.id.outlets);
+		outletNames.setDropDownViewResource(
+				android.R.layout.simple_spinner_dropdown_item);
+		outletNamesSpinner.setAdapter(outletNames);
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent data) {
+		if (requestCode == ORDER && resultCode == RESULT_OK) {
+			// TODO: get the OutletMobile from data
+		}
 	}
 }
