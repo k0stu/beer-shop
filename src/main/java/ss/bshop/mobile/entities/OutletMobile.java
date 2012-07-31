9a4f4ec3543ca5b019d9b5e33b62695868c2d34d
Vic <tokarevv@yahoo.com>
2012-07-31 15:18:26 +0300

    Entities not anootated

---
+++ src/main/java/ss/bshop/mobile/entities/OutletMobile.java
@@ -0,0 +1,41 @@
+package ss.bshop.mobile.entities;
+
+
+public class OutletMobile {
+
+	private Long id;
+	private String name;
+	private String address;
+	private String phone;
+	private Integer discount;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Integer getDiscount() {
+		return discount;
+	}
+	public void setDiscount(Integer discount) {
+		this.discount = discount;
+	}
+}
