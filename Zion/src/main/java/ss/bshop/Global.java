12ee307255415b87aae218162b82d801d7e6ab68
Kostya Tarasov <k0stu@github.com>
2012-07-31 14:56:42 +0300

    SDK15 config, new button

--- Zion/src/main/java/ss/bshop/Global.java
+++ Zion/src/main/java/ss/bshop/Global.java
@@ -12,4 +12,13 @@ public class Global {
 	 * the list and getting article by it's name from the list).
 	 */
 	public static Hashtable<String, ArticleMobile> goods = new Hashtable();
+	/**
+	 * objectStorage is here for passing objects between activities. The
+	 * problem is that to pass an object in a bundle it has to implement special
+	 * interfaces, while primitives are passed with no additional actions. So
+	 * you put your object in objectStorage, pass the key and get it in other
+	 * activity. Don't forget to remove object from the table when you extracted
+	 * it.
+	 */
+	public static Hashtable<String, Object> objectStorage = new Hashtable();
 }
